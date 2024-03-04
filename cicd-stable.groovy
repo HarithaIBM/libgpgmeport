a27c0be0fe4d48075ec5a216c5586067d5fdb2a8
Haritha D <harithamma.d@ibm.com>
2024-03-04 06:22:59 -0500

    Initial Commit

---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/libgpgmeport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libgpgmeport.git'), string(name: 'PORT_DESCRIPTION', value: 'GnuPG Made Easy (GPGME) is a C language library that allows to add' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
